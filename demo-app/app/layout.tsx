9b3e233917726b7923de3ee06d67eb5f6be517c0
ilmari-h <52321471+ilmari-h@users.noreply.github.com>
2026-01-30 19:02:21 +0100

    Favicon

--- demo-app/app/layout.tsx
+++ demo-app/app/layout.tsx
@@ -21,19 +21,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/icon-32x32.png",
       },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
